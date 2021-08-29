1a88b057b05d3218ed9cdcec55229724e352e996
Jeve Stobs <76224164+Jeve-Stobs@users.noreply.github.com>
2021-08-29 03:27:06 -0400

    init

---
+++ constants.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.EMBED_COLOR_BLACK =
+  exports.EMBED_COLOR_RED =
+  exports.EMBED_COLOR_ORANGE =
+  exports.EMBED_COLOR_YELLOW =
+  exports.EMBED_COLOR_GREEN =
+  exports.API_BASE =
+    void 0;
+exports.API_BASE = "https://srhpyqt94yxb.statuspage.io/api/v2/";
+exports.EMBED_COLOR_GREEN = "#57F287";
+exports.EMBED_COLOR_YELLOW = "#FEE75C";
+exports.EMBED_COLOR_ORANGE = "#F26522";
+exports.EMBED_COLOR_RED = "#ED4245";
+exports.EMBED_COLOR_BLACK = "#000000";
